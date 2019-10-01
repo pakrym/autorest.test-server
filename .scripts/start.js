6bbddb8d7a2a3c67e3e7f5b5876a9b430e3777cc
Garrett Serack <garretts@microsoft.com>
2019-10-01 13:02:35 -0700

    fix scripts

--- .scripts/start.js
+++ .scripts/start.js
@@ -1,2 +1,3 @@
+#!/usr/bin/env node
 p = require('child_process').spawn( process.execPath ,[`${__dirname}/../node_modules/wiremock/jdeploy-bundle/jdeploy.js`,'--root-dir', '.', '--port', '3000'],{ detatched: true,   stdio: 'ignore'} ) ; 
 setTimeout( () => { p.unref(); process.exit()},1000 )  
--- .scripts/stop.js
+++ .scripts/stop.js
@@ -1,1 +1,3 @@
+#!/usr/bin/env node
+
 require('http').request( {port: 3000, host:'localhost', method: 'POST', path: '/__admin/shutdown'} ).end()
